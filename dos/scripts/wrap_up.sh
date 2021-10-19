c7b24feb302c52590cb193f7eb1d5660fe745db4
Nicolò <vincinicolo98@gmail.com>
2021-10-19 20:40:59 +0200

    DOS - minifix wrap up

--- dos/scripts/wrap_up.sh
+++ dos/scripts/wrap_up.sh
@@ -14,12 +14,12 @@ ssh server.vinci-dos.offtech "sudo ./scripts/config/syn_cookies.sh $STATE"
 ETH=$(ssh client.vinci-dos.offtech "sudo ip route get 5.6.7.8" | awk -F " " '{print $5;}')
 
 # Start gathering data on Client
-echo "Start tcpdump on Client" &
-ssh client.vinci-dos.offtech "sudo ./scripts/sniffer/sniff.sh $FILE $ETH" &> /dev/null &
+echo "Start tcpdump on Client" 
+ssh client.vinci-dos.offtech "sudo ./scripts/sniffer/sniff.sh $FILE $ETH" &> /dev/null 
 
 # Start legitimate traffic
 echo "Start legitimate traffic"
-ssh client.vinci-dos.offtech "sudo ./scripts/traffic/leg_traffic.sh" &> /dev/null &
+ssh client.vinci-dos.offtech "sudo ./scripts/traffic/leg_traffic.sh" &> /dev/null 
 
 # Wait 30 seconds
 echo "Wait 30 seconds"
@@ -27,7 +27,7 @@ sleep 30
 
 # Start attack
 echo "Start attack"
-ssh attacker.vinci-dos.offtech "sudo ./scripts/traffic/attack.sh" &> /dev/null &
+ssh attacker.vinci-dos.offtech "sudo ./scripts/traffic/attack.sh" &> /dev/null 
 
 # Wait end
 echo "Wait end"
