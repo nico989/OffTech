4037e5ab8603d2afb56935cdd0284cea8b4dbd4a
Nicolò <vincinicolo98@gmail.com>
2021-11-10 11:06:52 +0100

    CTF resilient -add new tools (red) - setup server (blue)

--- ctfResilient/blue/setup_server.sh
+++ ctfResilient/blue/setup_server.sh
@@ -1,13 +1,11 @@
-sudo apt-get update 2> /dev/null
-sudo apt-get install apache2 -y 2> /dev/null
+#!/bin/bash
 
-1.html
-2.html
-3.html
-4.html
-5.html
-6.html
-7.html
-8.html
-9.html
-10.html
+echo "Installing Apache"
+sudo apt-get update > /dev/null
+sudo apt-get install apache2 -y > /dev/null
+
+echo "Creating html files"
+for i in {1..10}; 
+do 
+    sudo touch "/var/www/html/$i.html"; 
+done;
